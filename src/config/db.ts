2bbd923d39310fa1e2c87603bee0a8fa7e8cccb5
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-06-22 11:04:34 -0300

    Type accommodation

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 
 class Database {
     //private DB_URL = 'mongodb://link-db/db_portal';
-    private DB_URL = 'mongodb://localhost:27017/db_portal';
+    private DB_URL = 'mongodb://localhost:27017/lodge_dev';
 
     createConnection() {
         mongoose.connect(this.DB_URL);
