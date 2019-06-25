71f01da45778cb207b61694e87f5e6e4b1fe3734
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-06-24 21:16:01 -0300

    Consumption Item feature

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 class Database {
-    //private DB_URL = 'mongodb://link-db/db_portal';
+    
     private DB_URL = 'mongodb://localhost:27017/lodge_dev';
 
     createConnection() {
